a615a64bac3fbfc8bd5c73073c34357f00e59c6a
juanovni <constantinemurillo@gmail.com>
2017-12-20 23:58:47 -0500

    Added delete_setting_users function to delete users by id_user

--- controllers/setting_usersController.php
+++ controllers/setting_usersController.php
@@ -1,15 +1,23 @@
 <?php
 
-
 class setting_usersController extends Controller
 {
-	private $_usersList;
+	private $_usersSett;
+	private $_usersInfo;
+	private $_sess_client;
 
 	public function __construct()
 	{
 		parent::__construct();
-		$this->_usersList = $this->loadModel('setting_users');
-		Session::strictAccess(array('admin'));
+		$this->_usersSett = $this->loadModel('setting_users');
+		$this->_usersInfo = $this->loadModel('usersInfo');
+		Session::strictAccess(array('AD','MU'));
+		$this->_sess_client = Session::get('mb_id_client');
+		$this->_sess_user   = Session::get('mb_id_user');
+
+		$this->rs_UserSessioninfo = $this->_usersInfo->getUsersInfo(
+			$this->_sess_client, 
+			$this->_sess_user);
 		
 	}
 
@@ -19,27 +27,13 @@ class setting_usersController extends Controller
 		$this->_view->title			= 'Setting Users';
 		$this->_view->page_title	= 'Setting Users';
 
-		$arr_userList	= $this->_usersList->getUsersList(1);
+		$arr_userList	= $this->_usersSett->getUsersList($this->_sess_client);
 		$rs_userList 	= array();
 		if($this->isArray($arr_userList)){
 			foreach($arr_userList as $key => $val){
-				$icon_userType      = "icon-account";
-                $icon_userDelete    = "icon-delete";
-                $icon_userWebsite   = "icon-web";
-                $tooltip_userWebsite = "Click to edit the website for a user";
-                $tooltip_userDelete	 = "Click to delete a user";
-				$tooltip_userType	 = "Normal user";
-
-                if($val['acronym'] == 'admin'){
-                    $icon_userType      = "icon-account-multiple";
-                    $icon_userDelete    = "icon-lock";
-                    $icon_userWebsite   = "icon-account-network";
-                    $tooltip_userWebsite = "This user has all the websites enabled";
-                    $tooltip_userDelete	 = "You can't delete this user";
-                    $tooltip_userType	 = "Master user";
-				}
-
 				
+				$icon 	 = $this->getIconByLevel($val['acronym']);
+				$tooltip = $this->getTooltipByLevel($val['acronym']);
 
                 $rs_userList[$val['id_user']]['nick_name']	= $val['nick_name'];
                 $rs_userList[$val['id_user']]['id_user'] 	= $val['id_user'];
@@ -48,14 +42,13 @@ class setting_usersController extends Controller
                 $rs_userList[$val['id_user']]['lastName']	= $val['last_name'];
 				$rs_userList[$val['id_user']]['status']		= $val['status'];
 
+                $rs_userList[$val['id_user']]['icon_userType']		= $icon['type'];
+                $rs_userList[$val['id_user']]['icon_userDelete']	= $icon['delete'];
+                $rs_userList[$val['id_user']]['icon_userWebsite']	= $icon['website'];
 
-                $rs_userList[$val['id_user']]['icon_userType']		= $icon_userType;
-                $rs_userList[$val['id_user']]['icon_userDelete']	= $icon_userDelete;
-                $rs_userList[$val['id_user']]['icon_userWebsite']	= $icon_userWebsite;
-
-                $rs_userList[$val['id_user']]['tp_userWebsite']		= $tooltip_userWebsite;
-                $rs_userList[$val['id_user']]['tp_userDelete']		= $tooltip_userDelete;
-                $rs_userList[$val['id_user']]['tp_userType']		= $tooltip_userType;
+                $rs_userList[$val['id_user']]['tp_userWebsite']		= $tooltip['user_website'];
+                $rs_userList[$val['id_user']]['tp_userDelete']		= $tooltip['user_delete'];;
+                $rs_userList[$val['id_user']]['tp_userType']		= $tooltip['user_type'];;
 
 			}
 		}
@@ -65,11 +58,33 @@ class setting_usersController extends Controller
 
 	}
 
-	public function delete_setting_users($id_store)
+	public function delete_setting_users($id_user)
 	{
+		$iduser = $this->filterInt($id_user);
 
-		$this->_usersList->deleteSettingUsers(1, $id_store);
+		if(!$this->filterInt($id_user))
+		{
+			$this->redirect('setting_users');
+		}
+		
+		if(!$this->_usersInfo->getUsersExist($this->_sess_client, $iduser))
+		{
+			$this->redirect('setting_users');
+		}
+
+		$rsUserRol =  $this->_usersInfo->getUsersInfo($this->_sess_client, $iduser);
+		
+		if($rsUserRol['acronym'] == 'MU')
+		{
+			$this->redirect('setting_users');
+		}
+
+		$this->_usersSett->deleteUsersRol($this->_sess_client, $iduser);
+		$this->_usersSett->deleteUsers($iduser);
+		
+
 		$this->redirect('setting_users');
+
 	}
 
 }
