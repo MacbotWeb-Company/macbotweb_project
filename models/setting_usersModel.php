12e4e93c832e9b6a4a0fb66c31c7ddda3db3fadc
juanovni <constantinemurillo@gmail.com>
2017-12-21 00:00:58 -0500

    Added getUsersExist function

--- models/setting_usersModel.php
+++ models/setting_usersModel.php
@@ -36,10 +36,52 @@ class setting_usersModel extends Model
 		return $data;
 	}
 
-	public function deleteSettingUsers($id_client, $id_store)
+
+	public function deleteUsers($id_user)
 	{
+		$sql  = "DELETE FROM mb_users WHERE id_user = :id_user;";
+		
+		$this->_db->query($sql);
+		$this->_db->bind('id_user', $id_user);
+		$this->_db->execute();
 
 	}
+
+	public function deleteUsersRol($id_client, $id_user)
+	{
+		$sql  = "DELETE FROM mb_user_rol WHERE id_client = :id_client AND id_user = :id_user;";
+		
+		$this->_db->query($sql);
+		$this->_db->bind('id_client', $id_client);
+		$this->_db->bind('id_user', $id_user);
+		$this->_db->execute();
+
+	}
+
+	public function updateUsersRolStatus($id_client, $id_user, $status = 'A')
+	{
+		$sql  = "UPDATE mb_user_rol SET status = :status WHERE id_client = :id_client AND id_user = :id_user;";
+		
+		$this->_db->query($sql);
+		$this->_db->bind('status', $status);
+		$this->_db->bind('id_client', $id_client);
+		$this->_db->bind('id_user', $id_user);
+		$this->_db->execute();
+
+	}
+
+	public function updateUsersStatus($id_user, $status = 'A')
+	{
+
+		$sql  = "UPDATE mb_users SET status = :status WHERE id_user = :id_user;";
+		echo $sql;
+		$this->_db->query($sql);
+		$this->_db->bind('status', $status);
+		$this->_db->bind('id_user', $id_user);
+		$this->_db->execute();
+
+	}
+	
 
 
 }
