dd4144eaa35f9c004480924710e6f3c50dc052b4
juanovni <constantinemurillo@gmail.com>
2017-12-28 00:42:12 -0500

    Modified navbar class menu list

--- views/layout/default/menu_notifications.php
+++ views/layout/default/menu_notifications.php
@@ -149,31 +149,25 @@
     </nav>
 
     <div class="content">
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-color: #3c4252!important; border-radius: 0px !important;">
-        <i class="logo-icon icon-chart-line mr-2" style="color: #fff;"></i>
-        <a class="navbar-brand" href="#" style="font-weight: bold;"><?php echo $this->page_title ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
-                aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-color: #3c4252!important; border-radius: 0px !important;">
+            <i class="logo-icon <?php echo $this->icon_title;?> mr-2" style="color: #fff;"></i>
+            <a class="navbar-brand" href="#" style="font-weight: bold;"><?php echo $this->page_title ?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+                    aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Overviews <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Add<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Report</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
 
 
