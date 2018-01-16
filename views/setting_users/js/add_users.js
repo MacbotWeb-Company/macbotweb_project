07bbb1c28035d32a5b1ae950757330b7e4eaefba
Juan Jose <constantinemurillo@gmail.com>
2018-01-16 16:36:06 -0500

    Commented code

--- views/setting_users/js/add_users.js
+++ views/setting_users/js/add_users.js
@@ -1,16 +1,26 @@
 //$(document).ready(function() {
-	 (function () {
-        "use strict";
-        window.addEventListener("load", function () {
-            var form = document.getElementById("needs-validation");
-            form.addEventListener("submit", function (event) {
-                if ( form.checkValidity() == false )
-                {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add("was-validated");
-            }, false);
+/* (function () {
+    "use strict";
+    window.addEventListener("load", function () {
+        var form = document.getElementById("needs-validation");
+        form.addEventListener("submit", function (event) {
+            if ( form.checkValidity() == false )
+            {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+			var pass = $("#mb_PasswordConfirmation").val();
+			if(pass == 'juan')
+			{
+				alert("Hola");
+				event.preventDefault();
+                event.stopPropagation();
+			}
+
+            form.classList.add("was-validated");
         }, false);
-    }()); 
+    }, false);
+}()); */
+
+
 //});
